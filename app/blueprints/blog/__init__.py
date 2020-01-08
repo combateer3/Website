9cdd8f4ca3502dfffd2aff33d8fe2cad65960e4e
Scott <scottcorbat@yahoo.com>
2020-01-08 01:46:09 -0500

    Posts can now be deleted Delete button only shows for user who made the post Deletion performed through light weight form with hidden input to fetch postid Some extra styling added to the blog page

--- app/blueprints/blog/__init__.py
+++ app/blueprints/blog/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 from app import db
 from app.forms import PostForm
@@ -25,3 +25,14 @@ def post():
         return redirect(url_for('blog.blog_home'))
 
     return render_template('create_post.html', form=form)
+
+
+@blog.route('/delete_post', methods=['GET', 'POST'])
+@login_required
+def delete_post():
+    # not worth the hassle to create a Flask Form for this simple process
+    postid = request.form['post_del']
+    Post.query.filter_by(id=postid).delete()
+    db.session.commit()
+
+    return redirect(url_for('blog.blog_home'))
